f89bfc45b6c443f7973d1c428c4ca00a0d8dc93c
Nawaf Alsulami <nawafnaim@gmail.com>
2015-02-12 11:53:46 -0600

    cleaned up

--- lib/src/plural_verb.dart
+++ lib/src/plural_verb.dart
@@ -13,12 +13,6 @@ class PluralVerbEncoder extends Converter<String, String> {
     });
 
     [
-//      [r'$', (m) => 's'],
-//      [r'([^aeiou])y$', (m) => '${m[1]}ies'],
-//      [r'(z)$', (m) => '${m[1]}es'],
-//      [r'(ss|zz|x|h|o)$', (m) => '${m[1]}es'],
-//      [r'(ed)$', (m) => '${m[1]}']
-//      [r'(eats)', (m) => 'eat']//,
       [r'e?s$', (m) => ''],
       [r'ies$', (m) => 'y'],
       [r'([^h|z|o|i])es$', (m) => '${m[1]}e'],
@@ -33,24 +27,6 @@ class PluralVerbEncoder extends Converter<String, String> {
   void addInflectionRule(String singular, dynamic plural) {
     _inflectionRules.add([new RegExp(singular, caseSensitive: false), plural]);
   }
-
-//  void addIrregularInflectionRule(String singular, String plural) {
-//    addInflectionRule(singular, plural);
-//    final s0 = singular.substring(0, 1);
-//    final srest = singular.substring(1);
-//    final p0 = plural.substring(0, 1);
-//    final prest = plural.substring(1);
-//
-//    if (s0.toUpperCase() == p0.toUpperCase()) {
-//      addInflectionRule('(${s0})${srest}\$', (m) => '${m[1]}${prest}');
-//      addInflectionRule('(${p0})${prest}\$', (m) => '${m[1]}${prest}');
-//    } else {
-//      addInflectionRule('${s0.toUpperCase()}(?i)${srest}\$', (m) => '${p0.toUpperCase()}${prest}');
-//      addInflectionRule('${s0.toLowerCase()}(?i)${srest}\$', (m) => '${p0.toUpperCase()}${prest}');
-//      addInflectionRule('${p0.toUpperCase()}(?i)${prest}\$', (m) => '${p0.toUpperCase()}${prest}');
-//      addInflectionRule('${p0.toLowerCase()}(?i)${prest}\$', (m) => '${p0.toLowerCase()}${prest}');
-//    }
-//  }
 
   @override
   String convert(String word) {
